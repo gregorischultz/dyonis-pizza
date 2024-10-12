3e7e1eeddf949ea86844f8be08606a1b2122b353
Grégori <gregori@Mac-mini-de-Camila.local>
2024-10-12 17:08:46 +0200

    add page+componentes header fait

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Bem-vindo à Página Inicial!</h2>
+      <p>Este é o conteúdo da Home.</p>
+    </div>
+  );
+};
+
+export default Home;
