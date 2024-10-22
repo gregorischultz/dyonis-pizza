2698a3602bfea50fa416928138afe362eee53729
Grégori <gregori@Mac-mini-de-Camila.local>
2024-10-22 17:51:07 +0200

    change css component CardProduct

--- src/componentes/CardProduct.jsx
+++ src/componentes/CardProduct.jsx
@@ -10,11 +10,11 @@ const CardProduct = ({ image, name, price, description }) => {
     return (
         <div className="flex flex-col justify-start items-center gap-4 h-[405px] flex-1">
             <img src={image} alt={`${name} Card`} />
-            <div className="self-stretch flex justify-between items-start">
+            <div className="w-4/5 flex justify-between items-start">
                 <h3 className="p1">{name}</h3>
                 <p className="p1">{price}</p>
             </div>
-            <p className="self-stretch text-black text-sm font-montserrat font-normal leading-7 break-words">
+            <p className="w-4/5 text-black text-sm font-montserrat font-normal leading-7 break-words">
                 {description}
             </p>
         </div>
