3b0f8c614173b4312df925605a7187cccad30e48
Grégori <gregori@Mac-mini-de-Camila.local>
2024-10-22 14:11:40 +0200

    start of the CarteSection component, button structure ready

--- src/componentes/CarteSection.jsx
+++ src/componentes/CarteSection.jsx
@@ -1,11 +1,34 @@
 /*Componente da page Home.jsx*/
 
 import React from "react";
-import CardProduct from "./CardProduct";
 import pizzaCard from '../assets/pizzaCard.png'
 
 function CarteSection() {
-    
+    return (
+        <div className="h-full w-full px-20 py-10 flex flex-col justify-center items-end gap-10">
+            <h1 className="self-stretch text-center text-black text-[64px] font-alegreyaSans font-bold leadind-[64px] break-words">Carte</h1>
+            <h4 className="p">Découvrez nos pizzas base crème ou sauce tomate ainsi que nos paninis ! </h4>
+            <div className="self-stretch flex justify-center items-start gap-8">
+                <div className="div-button">
+                    <button className="button">Snacks</button>
+                </div>
+                <div className="div-button">
+                    <button className="button">Pizza</button>
+                </div>
+                <div className="div-button">
+                    <button className="button">Desserts</button>
+                </div>
+                <div className="div-button">
+                    <button className="button">Boissons</button>
+                </div>
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default CarteSection;
