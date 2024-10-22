149849c329469a544650387aec0c886347062544
Grégori <gregori@Mac-mini-de-Camila.local>
2024-10-22 16:03:57 +0200

    structure of the menu section of the home page done, the product card still needs to be aligned

--- src/componentes/CardProduct.jsx
+++ src/componentes/CardProduct.jsx
@@ -6,26 +6,26 @@ import PropTypes from 'prop-types';
 
 
 
-const CardProduct = ({ image, name, price, ingredients }) => {
+const CardProduct = ({ image, name, price, description }) => {
     return (
-        <div className="w-full h-full flex flex-col justify-start items-center gap-4">
+        <div className="flex flex-col justify-start items-center gap-4 h-[405px] flex-1">
             <img src={image} alt={`${name} Card`} />
             <div className="self-stretch flex justify-between items-start">
                 <h3 className="p1">{name}</h3>
                 <p className="p1">{price}</p>
             </div>
             <p className="self-stretch text-black text-sm font-montserrat font-normal leading-7 break-words">
-                {ingredients}
+                {description}
             </p>
         </div>
     );
 };
 
-CardPizza.propTypes = {
+CardProduct.propTypes = {
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
-    ingredients: PropTypes.string.isRequired
+    description: PropTypes.string.isRequired
 };
 
 export default CardProduct;
