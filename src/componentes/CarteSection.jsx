c0a0ba8858581ea2093f3943169857b48ca72054
Grégori <gregori@Mac-mini-de-Camila.local>
2024-10-25 14:44:27 +0200

    menu navigation butts transformed into a component

--- src/componentes/CarteSection.jsx
+++ src/componentes/CarteSection.jsx
@@ -3,18 +3,19 @@
 import React, { useState } from "react";
 import pizzaCard from '../assets/pizzaCard.png';
 import CardProduct from '../componentes/CardProduct';
+import NavCarte from "./NavCarte";
 
 
 const products = {
     pizzas: [
-        { id: 1, image:  pizzaCard , name: 'Pizza Margherita', price: '€ 6,00*', description: "Mozzarella, avec supplément basilic et huile d'olives" },
-        { id: 2, image:  pizzaCard , name: 'Porky', price: '€ 7,00*', description: "Mozzarella et jambon" },
-        { id: 3, image:  pizzaCard , name: 'Reine', price: '€ 9,00*', description: "Mozzarella et jambon" },
-        { id: 4, image:  pizzaCard , name: 'Vegan', price: '€ 9,00*', description: "Mozzarella et jambon" },
-        { id: 5, image:  pizzaCard , name: 'Modena', price: '€ 9,30*', description: "Mozzarella, jambon, œuf, olives et olives" },
-        { id: 6, image:  pizzaCard , name: 'Orientale', price: '€ 9,50*', description: "Mozzarella, merguez, poivrons, oignons et olives" },
-        { id: 7, image:  pizzaCard , name: 'Vegetarienne', price: '€ 9,90*', description: "Mozzarella, champignons, poivrons, artichauts, tomates, olives et basilic" },
-        { id: 8, image:  pizzaCard , name: 'Calzone', price: '€ 9,90*', description: "Mozzarella, jambon et œuf" },
+        { id: 1, image: pizzaCard, name: 'Pizza Margherita', price: '€ 6,00*', description: "Mozzarella, avec supplément basilic et huile d'olives" },
+        { id: 2, image: pizzaCard, name: 'Porky', price: '€ 7,00*', description: "Mozzarella et jambon" },
+        { id: 3, image: pizzaCard, name: 'Reine', price: '€ 9,00*', description: "Mozzarella et jambon" },
+        { id: 4, image: pizzaCard, name: 'Vegan', price: '€ 9,00*', description: "Mozzarella et jambon" },
+        { id: 5, image: pizzaCard, name: 'Modena', price: '€ 9,30*', description: "Mozzarella, jambon, œuf, olives et olives" },
+        { id: 6, image: pizzaCard, name: 'Orientale', price: '€ 9,50*', description: "Mozzarella, merguez, poivrons, oignons et olives" },
+        { id: 7, image: pizzaCard, name: 'Vegetarienne', price: '€ 9,90*', description: "Mozzarella, champignons, poivrons, artichauts, tomates, olives et basilic" },
+        { id: 8, image: pizzaCard, name: 'Calzone', price: '€ 9,90*', description: "Mozzarella, jambon et œuf" },
     ],
     boissons: [
         { id: 1, image: '', name: 'Badoit', price: '€ 1,50', description: "50CL" },
@@ -64,20 +65,7 @@ function CarteSection() {
         <div className="h-full w-full px-20 py-10 flex flex-col justify-center items-end gap-10">
             <h1 className="self-stretch text-center text-black text-[64px] font-alegreyaSans font-bold leadind-[64px] break-words">Carte</h1>
             <h4 className="p">Découvrez nos pizzas base crème ou sauce tomate ainsi que nos paninis ! </h4>
-            <div className="self-stretch flex justify-center items-start gap-8">
-                <div className="div-button">
-                    <button onClick={() => setCategory('snacks')} className="button">Snacks</button>
-                </div>
-                <div className="div-button">
-                    <button onClick={() => setCategory('pizzas')} className="button">Pizza</button>
-                </div>
-                <div className="div-button">
-                    <button onClick={() => setCategory('desserts')} className="button">Desserts</button>
-                </div>
-                <div className="div-button">
-                    <button onClick={() => setCategory('boissons')} className="button">Boissons</button>
-                </div>
-            </div>
+            <NavCarte onChangeCategory={setCategory} />
             <div className="self-stretch grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
                 {renderProducts()}
             </div>
