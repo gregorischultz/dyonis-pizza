cd6bbbee991684583e1656fa8bbce8e0bedb05e9
Grégori <gregori@Mac-mini-de-Camila.local>
2024-10-25 15:01:04 +0200

    hero section of carte page finished

--- src/pages/Carte.jsx
+++ src/pages/Carte.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Carte = () => {
   return (
-    <div>
-      <h1>Eu sou Carte</h1>
+    <div className='w-full h-full px-20 py-40 bg-creme flex flex-col justify-start items-center gap-10'>
+      <h1 className='w-[1042px] text-center text-footer-color text-6xl font-alegreyaSans font-bold leading-[64px]'>Découvrez nos pizzas base crème ou sauce tomate ainsi que nos paninis !</h1>
     </div>
   );
 };
