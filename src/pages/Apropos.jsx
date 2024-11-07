c6c528cba9fb9ebaef95d47f1f55594962ebd0d6
Grégori <gregorischultz07@gmail.com>
2024-11-07 15:31:16 +0100

    cardAvis component made, and add carroussel packages

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -33,31 +33,37 @@ const Apropos = () => {
         </div>
       </div>
       <div className="w-full h-full pt-10 pb-20 pl-20 pr-20 flex flex-col justify-start items-start gap-18">
-            <h1 className="text-black text-6xl font-alegreyaSans font-bold leading-[64px] break-words">Nos services</h1>
-            <div className="w-full px-[120px] py-4 flex justify-between items-start mt-[72px]">
-                <div className="div-cart-NosServices">
-                    <img src={emporter} alt="A emporter" />
-                    <div className="div-text">
-                        <h3 className="h3">À emporter</h3>
-                        <p className="p">Commandez par téléphone et <br /> rendez vous sur place pour <br /> récupérer votre commande.</p>
-                    </div>
-                </div>
-                <div className="div-cart-NosServices">
-                    <img src={evenements} alt="Événements privés" />
-                    <div className="div-text">
-                        <h3 className="h3">Événements privés</h3>
-                        <p className="p">Réservez pour un événement privé! <br /> Pour plus d’informations prenez <br /> contact avec nous.</p>
-                    </div>
-                </div>
-                <div className="div-cart-NosServices">
-                    <img src={terrasse} alt="Terrasse" />
-                    <div className="div-text">
-                        <h3 className="h3">Terrasse</h3>
-                        <p className="p">Profitez de votre repas sur <br /> place en intérieur climatisé, ou <br /> en terrasse !</p>
-                    </div>
-                </div>
+        <h1 className="text-black text-6xl font-alegreyaSans font-bold leading-[64px] break-words">Nos services</h1>
+        <div className="w-full px-[120px] py-4 flex justify-between items-start mt-[72px]">
+          <div className="div-cart-NosServices">
+            <img src={emporter} alt="A emporter" />
+            <div className="div-text">
+              <h3 className="h3">À emporter</h3>
+              <p className="p">Commandez par téléphone et <br /> rendez vous sur place pour <br /> récupérer votre commande.</p>
             </div>
+          </div>
+          <div className="div-cart-NosServices">
+            <img src={evenements} alt="Événements privés" />
+            <div className="div-text">
+              <h3 className="h3">Événements privés</h3>
+              <p className="p">Réservez pour un événement privé! <br /> Pour plus d’informations prenez <br /> contact avec nous.</p>
+            </div>
+          </div>
+          <div className="div-cart-NosServices">
+            <img src={terrasse} alt="Terrasse" />
+            <div className="div-text">
+              <h3 className="h3">Terrasse</h3>
+              <p className="p">Profitez de votre repas sur <br /> place en intérieur climatisé, ou <br /> en terrasse !</p>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className='w-full h-full pt-10 pb-20 px-20 flex flex-col justify-start items-start gap-[72px]'>
+        <h1 className='text-black text-[64px] font-alegreyaSans font-bold leading-[64px] break-words'>Ce que nos clients pensent de nous</h1>
+        <div className='w-full h-full px-20 py-4 flex justify-center items-center gap-8'>
+          
+        </div>
+      </div>
     </div>
 
   );
