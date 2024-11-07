f7dd93456088db590fd9d896e813e8b97a4721c0
Grégori <gregorischultz07@gmail.com>
2024-11-07 13:47:24 +0100

    second part of the Apropos page made

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -7,22 +7,22 @@ const Apropos = () => {
     <div className='w-full h-full flex flex-col justify-start items-start'>
       <div className="w-full h-full relative bg-creme flex justify-center items-center">
         <div className="text-center flex flex-col items-center mt-16">
-          <h1 className="absolute text-center text-footer-color text-7xl font-alegreyaSans font-bold">Découvrez nos offres et services</h1>
+          <h1 className="absolute text-center text-footer-color text-7xl font-alegreyaSans font-bold">Découvrez nos offres<br /> et services</h1>
           <div className="relative">
             <img src={imgPizza} alt="Pizza" className="w-full h-auto mt-36" />
           </div>
         </div>
       </div>
       <div className='w-full h-full pt-10 pb-20 px-20 flex flex-col justify-start items-start gap-[72px]'>
-        <h1 className='text-[#2D1312] text-[64px] font-alegreyaSans font-bold leading-[64px] break-words'>Notre spécialité</h1>
-        <div>
-          <div>
-            <h2>Pizza DUO</h2>
-            <p>Vous hésitez entre deux pizzas ? <br />Pas de soucis, choisissez votre pizza DUO et profitez de deux pizzas au même temps.</p>
+        <h1 className='text-black text-[64px] font-alegreyaSans font-bold leading-[64px] break-words'>Notre spécialité</h1>
+        <div className='self-stretch py-4 flex justify-start items-start gap-[120px]'>
+          <div className='flex-grow flex-shrink basis-0 flex flex-col justify-start items-start gap-3'>
+            <h2 className='h2Aprops'>Pizza DUO</h2>
+            <p className='pApropos'>Vous hésitez entre deux pizzas ? <br />Pas de soucis, choisissez votre pizza DUO et profitez de<br /> deux pizzas au même temps.</p>
           </div>
-          <div>
-            <h2>Carte de fidélité</h2>
-            <p>Commandez votre carte de fidélité et profitez d’une pizza Senior offerte au bout de 10 pizzas achetés !</p>
+          <div className='flex-grow flex-shrink basis-0 self-stretch flex flex-col justify-start items-start gap-3 ml-40'>
+            <h2 className='h2Aprops'>Carte de fidélité</h2>
+            <p className='pApropos'>Commandez votre carte de fidélité et profitez d’une<br /> pizza Senior offerte au bout de 10 pizzas achetés !</p>
           </div>
         </div>
       </div>
