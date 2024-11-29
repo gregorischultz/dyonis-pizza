ac26030ee88cd9206523b1ff00a7a10a0012b00f
Grégori <gregorischultz07@gmail.com>
2024-11-29 16:01:10 +0100

    popup made

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
+import Popup from "./Popup";
 import { Link } from "react-router-dom";
 
 import Logo from '../assets/LOGO.png'
 
 function Header() {
+    const [isPopupOpen, setIsPopupOpen] = useState(false); //estado para controlar o popup
+
+    const openPopup = () => { //funçao para abrir o popup
+        setIsPopupOpen(true)
+    };
+
+    const closePopup = () => { //funçao para fechar o popup
+        setIsPopupOpen(false)
+    }
+
     return (
         <header className="bg-creme py-5 px-20 w-full h-20 flex justify-between items-center fixed top-0 left-0 z-50">
             <div className="flex justify-start items-center gap-3">
@@ -16,10 +28,12 @@ function Header() {
                 <Link to="Apropos" className="li-link">À propos</Link>
             </nav>
             <div className="h-12 px-[30px] py-3 bg-black rounded-xl border border-[#1B1D16]">
-                <button className="text-white text-xl font-alegreyaSans font-bold leading-6 break-words">
+                <button className="text-white text-xl font-alegreyaSans font-bold leading-6 break-words" onClick={openPopup}>
                     Contactez nous
                 </button>
             </div>
+
+            {isPopupOpen && <Popup onClose={closePopup}/>} {/*vai redenrizar o popup*/}
         </header>
     );
 }
--- src/componentes/Popup.jsx
+++ src/componentes/Popup.jsx
@@ -2,32 +2,34 @@ import React from "react";
 import imagem from '../assets/IMGpopup.png';
 
 
-function Popup() {
+function Popup({ onClose }) {
     return (
-        <div>
-            <h1>Vous souhaitez commander ou réserver une table ?</h1>
-            <div>
-                <img src={imagem} alt="Imagem" />
-                <div>
-                    <h3>Appelez-nous au : <br />02 46 91 07 42</h3>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+            <div className="w-[800px] h-auto p-8 bg-white rounded-2xl overflow-hidden flex flex-col justify-start items-center gap-6">
+                <h1 className=" text-footer-color text-4xl font-alegreyaSans font-bold leading-[48px] break-words">Vous souhaitez commander ou réserver une table ?</h1>
+                <div className="flex justify-center items-center gap-16">
+                    <img src={imagem} alt="Imagem" />
                     <div>
-                        <h4>Pendant nos horaires d’ouverture :</h4>
+                        <h3 className="self-stretch pb-5 text-black text-2xl font-alegreyaSans font-bold leading-[30px] break-words">Appelez-nous au : <br />02 46 91 07 42</h3>
                         <div>
-                            <p>
-                                <span>Du mardi au jeudi :</span><br />
-                                <span>11:30 - 14:00 18:00 - 22:00</span><br /><br />
-                                <span>Du vendredi au samedi :</span><br />
-                                <span>11:30 - 14:00 18:00 - 23:00</span><br /><br />
-                                <span>Le dimanche :</span><br />
-                                <span>18:00 - 22:00</span>
-                            </p>
+                            <h4 className="text-black pb-2 text-[20px] font-alegreyaSans font-bold leading-[24px] break-words">Pendant nos horaires d’ouverture :</h4>
+                            <div>
+                                <p>
+                                    <span className="text-black text-sm font-montserrat font-bold leading-[18px] break-words">Du mardi au jeudi :</span><br />
+                                    <span>11:30 - 14:00 18:00 - 22:00</span><br /><br />
+                                    <span className="text-black text-sm font-montserrat font-bold leading-[18px] break-words">Du vendredi au samedi :</span><br />
+                                    <span>11:30 - 14:00 18:00 - 23:00</span><br /><br />
+                                    <span className="text-black text-sm font-montserrat font-bold leading-[18px] break-words">Le dimanche :</span><br />
+                                    <span>18:00 - 22:00</span>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <button>Fermer</button>
+                <div className="flex flex-col self-stretch h-[52px] justify-start items-end gap-[10px]">
+                    <div className="inline-flex justify-center items-center pl-[20px] pr-[20px] pt-[16px] pb-[16px] bg-[#1B1D16] rounded-[12px] border">
+                        <button className="text-white text-base font-montserrat font-medium leading-[20px] break-words" onClick={onClose}>Fermer</button>
+                    </div>
                 </div>
             </div>
         </div>
